69790276559c7e1f3877139e3869dded3bc444c2
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-08-26 18:58:53 -0400

    able to cover for queston 1

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,3 +1,19 @@
+
+var startQuizButtonEl = document.getElementById('startQuizButton');
+var quizDEl = document.querySelector('.middleContainer');
+var questonListEl = document.querySelector('.questonList');
+var multipleChoiceListEl = document.querySelectorAll('.answerbtn');
+var choiceRosponEl = document.getElementById('correctOrWrong');
+var scoreEl = document.getElementById('highScore');
+var headerScoreTextEl = document.getElementById('headerScoreText')
+
+var questionsArray = [
+"JavaScript written under which of the following tag",
+"A variable in JavaScript declared with which of the following keyword?",
+"Which of the followings are primitive data types in JavaScript?",
+"Which of the following is NOT a JavaScript object?",
+"Which of the following is NOT a correct way of declaring an array in JavaScript?"
+]
 var jsQuestion1 = {
     questionTitle: "JavaScript written under which of the following tag",
     multipleChoice: ['<JavaScript></JavaScript>','<script></script>', '<code></code>', '<head></head>'],
@@ -26,5 +42,38 @@ var jsQuestion5 = {
 
 //selecting dom element for question Title
 var questionTitleEl = document.getElementById('question');
-//2. modile the title
-questionTitleEl.textContent = jsQuestion1.questionTitle;
+
+
+startQuizButtonEl.addEventListener("click", ()=>{
+    startQuiz();
+})
+function startQuiz(){
+   
+    console.log("Entering pop up startQuiz function");
+    quizDEl.style.setProperty('visibility','hidden');
+    questonListEl.style.setProperty('visibility','visible');
+    choiceRosponEl.style.setProperty('visibility','visible');
+    var i = 0;
+    while (i < questionsArray.length){
+        questionTitleEl.textContent = questionsArray[i];
+        console.log(`i on ${questionsArray[i]}`);
+        if(questionsArray[i] === jsQuestion1.questionTitle){
+            multipleChoiceListEl.forEach(function(element, index){
+            element.textContent = jsQuestion1.multipleChoice[index];
+            element.addEventListener('click', function(){
+                if(jsQuestion1.correctAnswer == index){
+                    choiceRosponEl.textContent = "Correct";
+                    console.log("correct");
+                    scoreEl.textContent = 20;
+                }
+                else{
+                    choiceRosponEl.textContent = "Wrong";
+                }
+            })
+
+            
+         })
+        }
+        i++;
+    }
+}
