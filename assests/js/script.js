18fb84cf57c61b2e09aa48e14e92e134bd07d6ba
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-08-26 22:58:24 -0400

    working on dom of setting up the answers and questions

--- assests/js/script.js
+++ assests/js/script.js
@@ -11,30 +11,25 @@ var headerScoreTextEl = document.getElementById('headerScoreText')
 
 var questionsArray =[
     {
-    questionTitle:"JavaScript written under which of the following tag",
-    choiceList:['<JavaScript></JavaScript>','<script></script>', '<code></code>', '<head></head>'],
-    correctAnswer: 1,
+        questionTitle: "JavaScript written under which of the following tag",
+        choiceList:["<JavaScript></JavaScript>","<script></script>", "<code></code>", "<head></head>"],
+        correctAnswer: 2,
     },
-
-   {
-    questionTitle:"A variable in JavaScript declared with which of the following keyword?",
-    choiceList:['new','int','string','var'],
-    correctAnswer:3
+    
+    {
+        questionTitle: "A variable in JavaScript declared with which of the following keyword?",
+        choiceList:["new","int","string","var"],
+        correctAnswer: 3,
     },
     {
-    questionTitle:"Which of the followings are primitive data types in JavaScript?",
-    choiceList:['String','Number','Boolean','All of the above'],
-    correctAnswer:3
+        questionTitle: "Which of the followings are primitive data types in JavaScript?",
+        choiceList:["String","Number","Boolean","All of the above"],
+        correctAnswer: 3,
     },
     {
-    questionTitle:"Which of the following is NOT a JavaScript object?",
-    choiceList:['var obj ={}','var obj ={name: "Kabir"}','var obj={name = "Kabir"}','var obj = new Object()'],
-    correctAnswer:2
-    },
-    {
-    questionTitle:"Which of the following is NOT a correct way of declaring an array in JavaScript?",
-    choiceList:['var arr= [1,"two",3,4]', 'var arr= new Array()','var[] arr= new Number()[5]','None of the above'],
-    correctAnswer:2
+        questionTitle: "Which of the following is NOT a JavaScript object?",
+        choiceList: ["var obj ={}","var obj ={name: 'Kabir'}","var obj={name = 'Kabir'}","var obj = new Object()"],
+        correctAnswer: 2,
     }
 ]
 
@@ -53,17 +48,30 @@ function startQuiz(){
     
     //answebtn1.setProperty('visibility','visible');
     console.log("we are inside startQuiz function: set the visibility for elemnts");
- var i =0;
-    while (i < questionsArray.length){
-        console.log("we are inside startQuiz function: iterating through the questionsArray");
-        questionTitleEl.textContent = questionsArray[i].questionTitle;
-        console.log("we are inside startQuiz function: set the questionTitle by the iterative");
-        multipleChoiceListEl.forEach(function(element, index){
-           console.log(element, index);
-           element.textContent = questionsArray[i].choiceList[index];
+
+    for(i=0;i<questionsArray.length;i++){
+    console.log("we are inside startQuiz function: iterating through the questionsArray");
+    questionTitleEl.textContent = questionsArray[i].questionTitle;
+    console.log("we are inside startQuiz function: set the questionTitle by the iterative");
+    multipleChoiceListEl.forEach(function(element, index){
+       element.textContent = questionsArray[i].choiceList[index];
+      //console.log(questionsArray[i].choiceList[index], index);
+       element.addEventListener('click', function(){
+        console.log("I clicked on " + index);
+           console.log("my correct answer is " + questionsArray[i].correctAnswer );
+           if(questionsArray.correctAnswer == index){
+            choiceRosponEl.textContent = "Correct";
+            console.log("correct");
+            scoreEl.textContent = 20;
+        }
+        else{
+            choiceRosponEl.textContent = "Wrong";
+        }
+      
         })
-        i++;
+    })
     }
+    
  
 }
 
