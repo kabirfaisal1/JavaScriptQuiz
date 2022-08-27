106125f2c357c81bdf96c4aa8fb2f8ff1b63c8b3
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-08-26 21:47:21 -0400

    redid the loop

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,79 +1,69 @@
 
 var startQuizButtonEl = document.getElementById('startQuizButton');
-var quizDEl = document.querySelector('.middleContainer');
-var questonListEl = document.querySelector('.questonList');
+var quizInfoEl = document.querySelector('.middleContainer');
+var questonListEl=document.querySelector('.questonList')
+//selecting dom element for question Title
+var questionTitleEl = document.getElementById('question');
 var multipleChoiceListEl = document.querySelectorAll('.answerbtn');
 var choiceRosponEl = document.getElementById('correctOrWrong');
 var scoreEl = document.getElementById('highScore');
 var headerScoreTextEl = document.getElementById('headerScoreText')
 
-var questionsArray = [
-"JavaScript written under which of the following tag",
-"A variable in JavaScript declared with which of the following keyword?",
-"Which of the followings are primitive data types in JavaScript?",
-"Which of the following is NOT a JavaScript object?",
-"Which of the following is NOT a correct way of declaring an array in JavaScript?"
-]
-var jsQuestion1 = {
-    questionTitle: "JavaScript written under which of the following tag",
-    multipleChoice: ['<JavaScript></JavaScript>','<script></script>', '<code></code>', '<head></head>'],
-    correctAnswer: 1
-}
-var jsQuestion2 = {
-    questionTitle: "A variable in JavaScript declared with which of the following keyword?",
-    multipleChoice: ['new', 'int','string','var'],
-    correctAnswer: 3
-}
-var jsQuestion3 = {
-    questionTitle: "Which of the followings are primitive data types in JavaScript?",
-    multipleChoice: ['String', 'Number','Boolean','All of the above'],
-    correctAnswer: 3
-}
-var jsQuestion4 = {
-    questionTitle: "Which of the following is NOT a JavaScript object?",
-    multipleChoice: ['var obj ={}', 'var obj ={name: "Kabir"}','var obj={name = "Kabir"}','var obj = new Object()'],
-    correctAnswer: 2
-}
-var jsQuestion5 = {
-    questionTitle: "Which of the following is NOT a correct way of declaring an array in JavaScript?",
-    multipleChoice:  ['var arr= [1, "two",3,4]', 'var arr= new Array()','var[] arr= new Number()[5]','None of the above'],
-    correctAnswer: 2
-}
+var questionsArray =[
+    {
+    questionTitle:"JavaScript written under which of the following tag",
+    choiceList:['<JavaScript></JavaScript>','<script></script>', '<code></code>', '<head></head>'],
+    correctAnswer: 1,
+    },
 
-//selecting dom element for question Title
-var questionTitleEl = document.getElementById('question');
+   {
+    questionTitle:"A variable in JavaScript declared with which of the following keyword?",
+    choiceList:['new','int','string','var'],
+    correctAnswer:3
+    },
+    {
+    questionTitle:"Which of the followings are primitive data types in JavaScript?",
+    choiceList:['String','Number','Boolean','All of the above'],
+    correctAnswer:3
+    },
+    {
+    questionTitle:"Which of the following is NOT a JavaScript object?",
+    choiceList:['var obj ={}','var obj ={name: "Kabir"}','var obj={name = "Kabir"}','var obj = new Object()'],
+    correctAnswer:2
+    },
+    {
+    questionTitle:"Which of the following is NOT a correct way of declaring an array in JavaScript?",
+    choiceList:['var arr= [1,"two",3,4]', 'var arr= new Array()','var[] arr= new Number()[5]','None of the above'],
+    correctAnswer:2
+    }
+]
+
 
 
 startQuizButtonEl.addEventListener("click", ()=>{
     startQuiz();
 })
+
 function startQuiz(){
    
-    console.log("Entering pop up startQuiz function");
-    quizDEl.style.setProperty('visibility','hidden');
-    questonListEl.style.setProperty('visibility','visible');
-    choiceRosponEl.style.setProperty('visibility','visible');
-    var i = 0;
+    console.log("we are inside startQuiz function");
+    quizInfoEl.style.setProperty('visibility','hidden'); //hiding the info
+    questonListEl.style.setProperty('visibility','visible'); //displaying the question box
+    choiceRosponEl.style.setProperty('visibility','visible');//displaying the question footer
+    
+    //answebtn1.setProperty('visibility','visible');
+    console.log("we are inside startQuiz function: set the visibility for elemnts");
+ var i =0;
     while (i < questionsArray.length){
-        questionTitleEl.textContent = questionsArray[i];
-        console.log(`i on ${questionsArray[i]}`);
-        if(questionsArray[i] === jsQuestion1.questionTitle){
-            multipleChoiceListEl.forEach(function(element, index){
-            element.textContent = jsQuestion1.multipleChoice[index];
-            element.addEventListener('click', function(){
-                if(jsQuestion1.correctAnswer == index){
-                    choiceRosponEl.textContent = "Correct";
-                    console.log("correct");
-                    scoreEl.textContent = 20;
-                }
-                else{
-                    choiceRosponEl.textContent = "Wrong";
-                }
-            })
-
-            
-         })
-        }
+        console.log("we are inside startQuiz function: iterating through the questionsArray");
+        questionTitleEl.textContent = questionsArray[i].questionTitle;
+        console.log("we are inside startQuiz function: set the questionTitle by the iterative");
+        multipleChoiceListEl.forEach(function(element, index){
+           console.log(element, index);
+           element.textContent = questionsArray[i].choiceList[index];
+        })
         i++;
     }
+ 
 }
+
