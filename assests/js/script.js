e527fd12c05ea9c2f6c701c1d280a0fb537a26f6
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-08-28 16:43:16 -0400

    working on display of questions

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,77 +1,87 @@
 
-var startQuizButtonEl = document.getElementById('startQuizButton');
-var quizInfoEl = document.querySelector('.middleContainer');
-var questonListEl=document.querySelector('.questonList')
+var startQuizButtonEl = document.getElementById("startQuizButton");
+var quizInfoEl = document.querySelector(".middleContainer");
+var questonListEl = document.querySelector(".questonList")
 //selecting dom element for question Title
-var questionTitleEl = document.getElementById('question');
-var multipleChoiceListEl = document.querySelectorAll('.answerbtn');
-var choiceRosponEl = document.getElementById('correctOrWrong');
-var scoreEl = document.getElementById('highScore');
-var headerScoreTextEl = document.getElementById('headerScoreText')
-
-var questionsArray =[
+var questionTitleEl = document.getElementById("question");
+var multipleChoiceBtnEl = document.querySelectorAll(".answerbtn");
+var choiceRosponEl = document.getElementById("correctOrWrong");
+var scoreEl = document.getElementById("highScore");
+var headerScoreTextEl = document.getElementById("headerScoreText")
+var score = null;
+var questionsArray = [
     {
         questionTitle: "JavaScript written under which of the following tag",
-        choiceList:["<JavaScript></JavaScript>","<script></script>", "<code></code>", "<head></head>"],
-        correctAnswer: 2,
+        choiceList: ["<JavaScript></JavaScript>", "<script></script>", "<code></code>", "<head></head>"],
+        correctAnswer: 1
     },
-    
+
     {
         questionTitle: "A variable in JavaScript declared with which of the following keyword?",
-        choiceList:["new","int","string","var"],
-        correctAnswer: 3,
+        choiceList: ["new", "int", "string", "var"],
+        correctAnswer: 3
     },
     {
         questionTitle: "Which of the followings are primitive data types in JavaScript?",
-        choiceList:["String","Number","Boolean","All of the above"],
-        correctAnswer: 3,
+        choiceList: ["String", "Number", "Boolean", "All of the above"],
+        correctAnswer: 3
     },
     {
         questionTitle: "Which of the following is NOT a JavaScript object?",
-        choiceList: ["var obj ={}","var obj ={name: 'Kabir'}","var obj={name = 'Kabir'}","var obj = new Object()"],
-        correctAnswer: 2,
+        choiceList: ["var obj ={}", "var obj ={name: 'Kabir'}", "var obj={name = 'Kabir'}", "var obj = new Object()"],
+        correctAnswer: 2
     }
 ]
 
 
-
-startQuizButtonEl.addEventListener("click", ()=>{
+startQuizButtonEl.addEventListener("click", () => {
     startQuiz();
 })
 
-function startQuiz(){
-   
+function startQuiz() {
+    score = null;
+    point = 25;
     console.log("we are inside startQuiz function");
-    quizInfoEl.style.setProperty('visibility','hidden'); //hiding the info
-    questonListEl.style.setProperty('visibility','visible'); //displaying the question box
-    choiceRosponEl.style.setProperty('visibility','visible');//displaying the question footer
-    
-    //answebtn1.setProperty('visibility','visible');
+    quizInfoEl.style.setProperty("visibility", "hidden"); //hiding the info
+    questonListEl.style.setProperty("visibility", "visible"); //displaying the question box
+    choiceRosponEl.style.setProperty("visibility", "visible");//displaying the question footer
+
+    //answebtn1.setProperty("visibility","visible");
     console.log("we are inside startQuiz function: set the visibility for elemnts");
 
-    for(i=0;i<questionsArray.length;i++){
-    console.log("we are inside startQuiz function: iterating through the questionsArray");
-    questionTitleEl.textContent = questionsArray[i].questionTitle;
-    console.log("we are inside startQuiz function: set the questionTitle by the iterative");
-    multipleChoiceListEl.forEach(function(element, index){
-       element.textContent = questionsArray[i].choiceList[index];
-      //console.log(questionsArray[i].choiceList[index], index);
-       element.addEventListener('click', function(){
-        console.log("I clicked on " + index);
-           console.log("my correct answer is " + questionsArray[i].correctAnswer );
-           if(questionsArray.correctAnswer == index){
-            choiceRosponEl.textContent = "Correct";
-            console.log("correct");
-            scoreEl.textContent = 20;
-        }
-        else{
-            choiceRosponEl.textContent = "Wrong";
-        }
-      
+    for (var i = 0; i < questionsArray.length; i--) {
+        console.log("we are inside startQuiz function: iterating through the questionsArray");
+
+        questionTitleEl.textContent = questionsArray[i].questionTitle;
+        console.log("we are inside startQuiz function: set the questionTitle by the iterative");
+
+        multipleChoiceBtnEl.forEach(function (element, index) {
+            element.textContent = questionsArray[i].choiceList[index];
+            console.log(questionsArray[i].choiceList[index]);
         })
-    })
+
+        var answer = questionsArray[i].correctAnswer;
+        var answerChoiceListEl = document.querySelector(".answerList");
+        answerChoiceListEl.addEventListener("click", function (event) {
+            var element = event.target;
+            if (element.matches(".answerbtn")) {
+                var dataNumber = parseInt(element.getAttribute("data-number"));
+
+                console.log(`inside userSelect function: ${dataNumber}`);
+                if (dataNumber == answer) {
+                    choiceRosponEl.textContent = "Correct";
+                    console.log("correct");
+                    score++;
+                    scoreEl.textContent = score * point;
+                }
+                else {
+                    choiceRosponEl.textContent = "Wrong";
+                }
+            }
+        })
+
+
     }
-    
- 
+
 }
 
